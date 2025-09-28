8a2674b859bdd243adaca5db13f9e25c0abe8e3b
Yan <yan.861@hotmail.com>
2025-09-28 20:32:32 -0300

    Estudando sobre o AppSettings.json, todas as anotações estão no Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+var prop = builder.Configuration.GetSection("Prop1").Value; // para acessar valores no appsettings.json
+
+var warning = builder.Configuration.GetSection("Logging").GetSection("LogLevel").GetSection("Default").Value;
+// para acessar propriedades de objetos complexos é necessario mais de uma chamada
+
+warning = builder.Configuration.GetValue<string>("Logging:LogLevel:Default");
+// também pode ser acessado desse jeito
+
+var myClass = builder.Configuration.GetSection("MyClass").Get<MyFirstApi.MyClass>();
+// tambem pode ser chamado diretamente para um objeto, ele vai até o appSettings e da mais preferencia para o mais especifico, ou seja
+// caso o mesmo objeto esteja no appSettings e no appSettings.Dev, ele vai sobreescrever com os valores do .Dev
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
